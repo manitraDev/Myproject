dfff8b79da9dd78597d8aa57cb3844e4924e4ebb
faby-dev <fabyandriamparany.7gmail.com>
2024-02-18 17:29:51 +0300

    view omena an'i manitra

--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -6,13 +6,9 @@
             <div class="relative flex-[1_auto] flex flex-col break-words min-w-0 bg-clip-border rounded-[.95rem] bg-white m-5">
                 <div class="relative flex flex-col min-w-0 break-words border border-dashed bg-clip-border rounded-2xl border-stone-200 bg-light/30">
                     <!-- card header -->
-                    <div class="px-9 pt-5 flex justify-between items-stretch flex-wrap min-h-[70px] pb-0 bg-transparent">
-                        <h3 class="flex flex-col items-start justify-center m-2 ml-0 font-medium text-xl/tight text-dark">
-                            <span class="mr-3 font-semibold text-dark">Projects Deliveries</span>
-                            <span class="mt-1 font-medium text-secondary-dark text-lg/normal">All projects from the Loopple team</span>
-                        </h3>
+                    <div class="px-9 pt-5 flex justify-end items-stretch flex-wrap min-h-[70px] pb-0 bg-transparent">
                         <div class="relative flex flex-wrap items-center my-2">
-                            <a href="javascript:void(0)" class="inline-block text-[.925rem] font-medium leading-normal text-center align-middle cursor-pointer rounded-2xl transition-colors duration-150 ease-in-out text-light-inverse bg-light-dark border-light shadow-none border-0 py-2 px-5 hover:bg-secondary active:bg-light focus:bg-light"> See other projects </a>
+                            <a href="#" class="bg-blue-800 w-48 h-10 rounded text-white font-bold flex items-center justify-center"> ajouter un nouve produit </a>
                         </div>
                     </div>
                     <!-- end card header -->
@@ -31,32 +27,22 @@
                                 </thead>
                                 <tbody>
                                 @foreach($produts as $produt)
-                                    {{ $produt.name }}
                                     <tr class="border-b border-dashed last:border-b-0">
-                                        <td class="p-3 pl-0">
-                                            <div class="flex items-center">
-                                                <div class="relative inline-block shrink-0 rounded-2xl me-3">
-                                                    <img src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/riva-dashboard-tailwind/img/img-49-new.jpg" class="w-[50px] h-[50px] inline-block shrink-0 rounded-2xl" alt="">
-                                                </div>
-                                                <div class="flex flex-col justify-start">
-                                                    <a href="javascript:void(0)" class="mb-1 font-semibold transition-colors duration-200 ease-in-out text-lg/normal text-secondary-inverse hover:text-primary"> Social Media API </a>
-                                                </div>
-                                            </div>
+                                        <td class="p-3 pr-0 text-end">
+                                            <span class="font-semibold text-light-inverse text-md/normal">{{ $produt->name }}</span>
                                         </td>
                                         <td class="p-3 pr-0 text-end">
-                                            <span class="font-semibold text-light-inverse text-md/normal">Olivia Cambell</span>
+                                            <span class="font-semibold text-light-inverse text-md/normal">{{ $produt->description }}</span>
                                         </td>
                                         <td class="p-3 pr-0 text-end">
-                    <span class="text-center align-baseline inline-flex px-2 py-1 mr-auto items-center font-semibold text-base/none text-success bg-success-light rounded-lg">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
-                      </svg> 6.5% </span>
+                                            @if($produt->isPublished === 1)
+                                                <span class="inline-block w-2 h-2 rounded-full bg-green-600"></span>
+                                            @else
+                                                <span class="inline-block w-2 h-2 rounded-full bg-red-600"></span>
+                                            @endif
                                         </td>
-                                        <td class="p-3 pr-12 text-end">
-                                            <span class="text-center align-baseline inline-flex px-4 py-3 mr-auto items-center font-semibold text-[.95rem] leading-none text-primary bg-primary-light rounded-lg"> In Progress </span>
-                                        </td>
-                                        <td class="pr-0 text-start">
-                                            <span class="font-semibold text-light-inverse text-md/normal">2023-08-23</span>
+                                        <td class="p-3 pr-0 text-end">
+                                            <span class="font-semibold text-light-inverse text-md/normal">{{ $produt->prix }}</span>
                                         </td>
                                         <td class="p-3 pr-0 text-end">
                                             <button class="ml-auto relative text-secondary-dark bg-light-dark hover:text-primary flex items-center h-[25px] w-[25px] text-base font-medium leading-normal text-center align-middle cursor-pointer rounded-2xl transition-colors duration-200 ease-in-out shadow-none border-0 justify-center">
